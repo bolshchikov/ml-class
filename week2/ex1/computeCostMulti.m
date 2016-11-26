9fdaccf0d2ac0c18a63a476700aa79d3a8c9e03a
Sergey Bolshchikov <sergey@bolshchikov.net>
2016-11-26 16:05:10 +0200

    multiple gradient decent

--- week2/ex1/computeCostMulti.m
+++ week2/ex1/computeCostMulti.m
@@ -7,13 +7,21 @@ function J = computeCostMulti(X, y, theta)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+part1 = 0;
+for i = 1:m
+    hx = 0;
+    for z = 1:size(X, 2)
+        hx = hx + X(i,z)*theta(z);
+    end
+    part1 = part1 + (hx-y(i))^2;
+end;
 
+J = part1/(2*m);
 
 
 
--- week2/ex1/gradientDescentMulti.m
+++ week2/ex1/gradientDescentMulti.m
@@ -4,7 +4,7 @@ function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters
 %   taking num_iters gradient steps with learning rate alpha
 
 % Initialize some useful values
-m = length(y); % number of training examples
+rows_length = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
@@ -18,12 +18,21 @@ for iter = 1:num_iters
     %
 
 
-
-
-
-
-
-
+    columns_length = size(X, 2);
+    new_thetas = zeros(columns_length, 1);
+    for n = 1:columns_length
+        upper_part = 0;
+        for m = 1:rows_length
+            hx = 0;
+            for z = 1:columns_length
+                hx = hx+ X(m,z)*theta(z);
+            end;
+            upper_part = upper_part + (hx - y(m))*X(m,n);
+        end;
+        new_thetas(n) = theta(n) - (alpha*upper_part)/rows_length;
+    end;
+    
+theta = new_thetas;
 
 
 
